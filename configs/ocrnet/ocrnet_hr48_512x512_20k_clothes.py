b21ad7087b7db86fa413c92e9d663b5386937c19
Adva Zair <adva@lightricks.com>
2020-08-25 09:22:22 +0300

    config fix.

--- configs/ocrnet/ocrnet_hr48_512x512_20k_clothes.py
+++ configs/ocrnet/ocrnet_hr48_512x512_20k_clothes.py
@@ -1,39 +1,40 @@
-# Copyright (c) 2019 Lightricks. All rights reserved.
-_base_ = "./ocrnet_hr48_512x512_20k_voc12aug.py"
+_base_ = './ocrnet_hr18_512x512_20k_clothes.py'
 norm_cfg = dict(type="BN", requires_grad=True)
-dataset_type = "ClothesDataset"
-data_root = "/data"
-ann_dir = ["mhp/LV-MHP-v2/masks"]
-img_dir = ["mhp/LV-MHP-v2/images"]
 model = dict(
-    backbone=dict(norm_cfg=norm_cfg,),
-    decode_head=[dict(norm_cfg=norm_cfg, num_classes=3,), dict(norm_cfg=norm_cfg, num_classes=3)],
-)
-data = dict(
-    samples_per_gpu=4,
-    workers_per_gpu=4,
-    train=dict(
-        type=dataset_type,
-        data_root=data_root,
-        img_dir=img_dir,
-        ann_dir=ann_dir,
-        split="LV-MHP-v2/splits/train.txt",
-    ),
-    val=dict(
-        type=dataset_type,
-        data_root=data_root,
-        img_dir=img_dir,
-        ann_dir=ann_dir,
-        split="LV-MHP-v2/splits/val.txt",
-    ),
-    test=dict(
-        type=dataset_type,
-        data_root=data_root,
-        img_dir=img_dir,
-        ann_dir=ann_dir,
-        split="LV-MHP-v2/splits/val.txt",
-    ),
-)
-load_from = "/data/clothes_seg_args/pretrained_models/ocrnet_hr48_512x512_20k_voc12aug/ocrnet_hr48_512x512_20k_voc12aug_20200617_233932-9e82080a.pth"
-work_dir = "/cnvrg/output/ocrnet_hr48_512x512_20k_mhp"
-gpu_ids = range(1)
+    pretrained='open-mmlab://msra/hrnetv2_w48',
+    backbone=dict(
+        norm_cfg=norm_cfg,
+        extra=dict(
+            stage2=dict(num_channels=(48, 96)),
+            stage3=dict(num_channels=(48, 96, 192)),
+            stage4=dict(num_channels=(48, 96, 192, 384)))),
+    decode_head=[
+        dict(
+            type='FCNHead',
+            in_channels=[48, 96, 192, 384],
+            channels=sum([48, 96, 192, 384]),
+            input_transform='resize_concat',
+            in_index=(0, 1, 2, 3),
+            kernel_size=1,
+            num_convs=1,
+            norm_cfg=norm_cfg,
+            concat_input=False,
+            dropout_ratio=-1,
+            num_classes=3,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+        dict(
+            type='OCRHead',
+            in_channels=[48, 96, 192, 384],
+            channels=512,
+            ocr_channels=256,
+            input_transform='resize_concat',
+            in_index=(0, 1, 2, 3),
+            norm_cfg=norm_cfg,
+            dropout_ratio=-1,
+            num_classes=3,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0))
+    ])
